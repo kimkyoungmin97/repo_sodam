1c85772116c3a2d1ec06f190048977aebc50d587
sung0615 <kk1hhh1fff1@naver.com>
2025-05-13 11:42:21 +0900

    로그인후 My 페이지 리뷰 내역 리스트 업데이트 삭제 완료

--- sodam/src/main/java/com/a5a5lab/module/user/review/ReviewDao.java
+++ sodam/src/main/java/com/a5a5lab/module/user/review/ReviewDao.java
@@ -1,5 +1,18 @@
 package com.a5a5lab.module.user.review;
 
-public interface ReviewDao {
+import java.util.List;
 
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface ReviewDao {
+	
+	//리뷰 리스트 보여주기
+	public List<ReviewDto> reviewListByUser(int memSeq);
+	
+	
+	//리뷰 My 페이지 업데이트 삭제
+	public int uelete(List<Long> deleteIds);
+	
 }
--- sodam/src/main/java/com/a5a5lab/module/user/review/ReviewService.java
+++ sodam/src/main/java/com/a5a5lab/module/user/review/ReviewService.java
@@ -1,5 +1,25 @@
 package com.a5a5lab.module.user.review;
 
-public class ReviewService {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ReviewService {
+	
+	
+	@Autowired
+	ReviewDao reviewDao;
+	
+	//리뷰 리스트 보여주기
+	public List<ReviewDto> reviewListByUser(int memSeq){
+		return reviewDao.reviewListByUser(memSeq);
+	}
+	
+	// My 페이지 리뷰 업데이트 삭제
+	public int uelete(List<Long> deleteIds) {
+		return reviewDao.uelete(deleteIds);
+	}
+	
 }
